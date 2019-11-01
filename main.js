4f4dcf51bc4c2f41f48d7152e11612c612070120
Stephanie Samson <sasamson98@hotmail.com>
2019-11-01 11:36:26 -0400

    updated code with ES6

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // Select input field
-var input = document.querySelector("#input");
+const input = document.querySelector('#input');
 
-function convert() {
-    var output = document.querySelector("#output");
+const convert = () => {
+    const output = document.querySelector('#output');
 
-    if (input.value !== "") {
-        output.style.display = "block";
+    if (input.value !== '') {
+        output.style.display = 'block';
     } else {
-        output.style.display = "none";
+        output.style.display = 'none';
     }
 
     // gram
-    document.querySelector("#gram").textContent = input.value * 453.592;
+    document.querySelector('#gram').textContent = input.value * 453.592;
     // kilogram
-    document.querySelector("#kilogram").textContent = (input.value * 453.592) / 1000;
+    document.querySelector('#kilogram').textContent = (input.value * 453.592) / 1000;
     // ounce
-    document.querySelector("#ounce").textContent = input.value * 16;
-}
+    document.querySelector('#ounce').textContent = input.value * 16;
+};
 
-input.addEventListener("input", convert);
+input.addEventListener('input', convert);
